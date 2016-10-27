544cd9b480a6dda954e36816f3c8a4f443bc3f7f
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2016-10-27 16:50:36 +0700

    this one was about how containers figure out the types of the objects you give them.
    Also shows us the logic on how it finds the correct params for constructors with parameters.

---
+++ ReflectIt/Container.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectIt
+{
+    public class Container
+    {
+        Dictionary<Type, Type> _map = new Dictionary<Type, Type>();
+        public ContainerBuilder For<TSource>()
+        {
+            return For(typeof(TSource));
+        }
+        public ContainerBuilder For(Type sourceType)
+        {
+            return new ContainerBuilder(this, sourceType);
+        }
+
+        public TSource Resolve<TSource>()
+        {
+            return (TSource)Resolve(typeof(TSource));
+        }
+        public object Resolve(Type sourceType)
+        {
+            if (_map.ContainsKey(sourceType))
+            {
+                var destinationType = _map[sourceType];
+                return CreateInstance(destinationType);
+            }
+            else
+            {
+                throw new InvalidOperationException("Could Not resolve" + sourceType.FullName);
+            }
+        }
+
+        private object CreateInstance(Type destinationType)
+        {
+            var parameters = destinationType.GetConstructors()
+                                            .OrderByDescending(c => c.GetParameters().Count())
+                                            .First()
+                                            .GetParameters()
+                                            .Select(p => Resolve(p.ParameterType))
+                                            .ToArray();
+
+            return Activator.CreateInstance(destinationType,parameters);
+        }
+
+        public class ContainerBuilder
+        {
+            public ContainerBuilder(Container container, Type sourceType)
+            {
+                _Container = container;
+                _sourceType = sourceType;
+            }
+
+            Container _Container;
+            Type _sourceType;
+
+            public ContainerBuilder Use<TDestination>()
+            {
+                return Use(typeof(TDestination));
+            }
+            public ContainerBuilder Use(Type destinationType)
+            {
+                _Container._map.Add(_sourceType, destinationType);
+                return this;
+            }
+        }
+    }
+
+
+}
--- ReflectItTests/IOCTests.cs
+++ ReflectItTests/IOCTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReflectIt;
 
 namespace ReflectItTests
 {
@@ -7,8 +8,29 @@ namespace ReflectItTests
     public class IoCTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Resolve_Types()
         {
+            var ioc = new Container();
+
+            var containerBuilderObj = ioc.For<ILogger>(); //This line of code creates a container bulder object with the source type (Ilogger) and a reference to the container object.
+
+            containerBuilderObj.Use<SqlSercerLogger>(); // This line of code registers the same container object with a dictionary entry of Ilogger type and SqlServer type.
+
+            var logger = ioc.Resolve<ILogger>(); // This returns an instance of the ILogger type.
+
+            Assert.AreEqual(typeof(SqlSercerLogger), logger.GetType()); // This is checking if the type of SqlServerLogger and Logger are both Logger type.
+        }
+
+        [TestMethod]
+        public void Can_Resolve_Types_Without_Default_Ctor()
+        {
+            var ioc = new Container();
+            ioc.For<ILogger>().Use<SqlSercerLogger>();
+            ioc.For <IRepository<Employee>>().Use<SqlRepository<Employee>>();
+
+            var repository = ioc.Resolve<IRepository<Employee>>();
+
+            Assert.AreEqual(typeof(SqlRepository<Employee>), repository.GetType());
         }
     }
 }
