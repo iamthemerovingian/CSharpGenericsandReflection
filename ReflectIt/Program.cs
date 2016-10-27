03a2aa55f0fd4a498de411697dd8a27a8138e14f
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2016-10-27 14:13:35 +0700

    Learning about inversion of control and how to make your own Containers.

--- ReflectIt/Program.cs
+++ ReflectIt/Program.cs
@@ -10,6 +10,36 @@ namespace ReflectIt
     {
         static void Main(string[] args)
         {
+            var empoyeeList = CreateCollection(typeof(List<>),typeof(List<Employee>));
+            Console.Write(empoyeeList.GetType().Name);
+
+            var genericArguments = empoyeeList.GetType().GenericTypeArguments;
+            foreach (var argument in genericArguments)
+            {
+                Console.WriteLine("[{0}]",argument.Name);
+            }
+
+            var employee = new Employee();
+            var employeeType = typeof(Employee);
+            var methodInfo = employeeType.GetMethod("Speak");
+            methodInfo = methodInfo.MakeGenericMethod(typeof(DateTime));
+            methodInfo.Invoke(employee, null);
+        }
+
+        private static object CreateCollection(Type collectionType, Type itemType)
+        {
+            var closedType = collectionType.MakeGenericType(itemType);
+            return Activator.CreateInstance(closedType);
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public void Speak<T>()
+        {
+            Console.WriteLine(typeof(T).Name);
         }
     }
 }
---
+++ ReflectItTests/IOCTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReflectItTests
+{
+    [TestClass]
+    public class IoCTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
